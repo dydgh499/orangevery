765f09bf08056c93d15c0f18358043ae0da8d96a
dydgh499 <dydgh499@naver.com>
2023-08-13 16:40:09 +0900

    feat: path view

--- resources/ts/@layouts/utils.ts
+++ resources/ts/@layouts/utils.ts
@@ -8,18 +8,18 @@ export const openGroups = ref<string[]>([])
  * @param {Object, String} item navigation routeName or route Object provided in navigation data
  */
 export const getComputedNavLinkToProp = computed(() => (link: NavLink) => {
-  const props: NavLinkProps = {
-    target: link.target,
-    rel: link.rel,
-  }
+    const props: NavLinkProps = {
+        target: link.target,
+        rel: link.rel,
+    }
 
-  // If route is string => it assumes string is route name => Create route object from route name
-  // If route is not string => It assumes it's route object => returns passed route object
-  if (link.to)
-    props.to = typeof link.to === 'string' ? { name: link.to } : link.to
-  else props.href = link.href
+    // If route is string => it assumes string is route name => Create route object from route name
+    // If route is not string => It assumes it's route object => returns passed route object
+    if (link.to)
+        props.to = typeof link.to === 'string' ? { name: link.to } : link.to
+    else props.href = link.href
 
-  return props
+    return props
 })
 
 /**
@@ -29,32 +29,47 @@ export const getComputedNavLinkToProp = computed(() => (link: NavLink) => {
  * @param {Object, String} link navigation link object/string
  */
 export const resolveNavLinkRouteName = (link: NavLink, router: Router) => {
-  if (!link.to)
-    return null
+    if (!link.to)
+        return null
 
-  if (typeof link.to === 'string')
-    return link.to
+    if (typeof link.to === 'string')
+        return link.to
 
-  return router.resolve(link.to).name
+    return router.resolve(link.to).name
 }
 
+export const extendRouterValidate = (regex: RegExp, curr_path_name: string) => {
+    if (curr_path_name.length) {       
+        if (regex.test(curr_path_name)) {
+            return true
+        }
+    }
+    return false
+}
 /**
  * Check if nav-link is active
  * @param {Object} link nav-link object
  */
 export const isNavLinkActive = (link: NavLink, router: Router) => {
-  // Matched routes array of current route
-  const matchedRoutes = router.currentRoute.value.matched
+    // Matched routes array of current route
+    const matchedRoutes = router.currentRoute.value.matched
 
-  // Check if provided route matches route's matched route
-  const resolveRoutedName = resolveNavLinkRouteName(link, router)
+    // Check if provided route matches route's matched route
+    const resolveRoutedName = resolveNavLinkRouteName(link, router)
 
-  if (!resolveRoutedName)
-    return false
+    if (!resolveRoutedName)
+        return false
 
-  return matchedRoutes.some(route => {
-    return route.name === resolveRoutedName || route.meta.navActiveLink === resolveRoutedName
-  })
+    let curr_route_name = router.currentRoute.value.fullPath.replace(/\//g, '-').split('?')[0]
+    curr_route_name = curr_route_name.startsWith('-') ? curr_route_name.substring(1) : curr_route_name
+    
+    const full_route_name = resolveRoutedName.toString()
+    const regexPattern = `^(${full_route_name}-create|${full_route_name}-edit-[0-9]+|${full_route_name}-part-[0-9]+|${full_route_name}-reply-[0-9]+)$`;
+    const regex = new RegExp(regexPattern);
+
+    return matchedRoutes.some(route => {
+        return route.name === resolveRoutedName || route.meta.navActiveLink === resolveRoutedName || extendRouterValidate(regex, curr_route_name)
+    })
 }
 
 /**
@@ -62,14 +77,14 @@ export const isNavLinkActive = (link: NavLink, router: Router) => {
  * @param {Array} children Group children
  */
 export const isNavGroupActive = (children: (NavLink | NavGroup)[], router: Router): boolean =>
-  children.some(child => {
-    // If child have children => It's group => Go deeper(recursive)
-    if ('children' in child)
-      return isNavGroupActive(child.children, router)
+    children.some(child => {
+        // If child have children => It's group => Go deeper(recursive)
+        if ('children' in child)
+            return isNavGroupActive(child.children, router)
 
-    // else it's link => Check for matched Route
-    return isNavLinkActive(child, router)
-  })
+        // else it's link => Check for matched Route
+        return isNavLinkActive(child, router)
+    })
 
 /**
  * Convert Hex color to rgb
@@ -77,14 +92,14 @@ export const isNavGroupActive = (children: (NavLink | NavGroup)[], router: Route
  */
 
 export const hexToRgb = (hex: string) => {
-// Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
-  const shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i
+    // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+    const shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i
 
-  hex = hex.replace(shorthandRegex, (m: string, r: string, g: string, b: string) => {
-    return r + r + g + g + b + b
-  })
+    hex = hex.replace(shorthandRegex, (m: string, r: string, g: string, b: string) => {
+        return r + r + g + g + b + b
+    })
 
-  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
+    const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
 
-  return result ? `${parseInt(result[1], 16)},${parseInt(result[2], 16)},${parseInt(result[3], 16)}` : null
+    return result ? `${parseInt(result[1], 16)},${parseInt(result[2], 16)},${parseInt(result[3], 16)}` : null
 }
