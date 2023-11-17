81954b575827586152a2590ac2df65f56400fba0
dydgh499 <dydgh499@naver.com>
2023-11-18 01:57:40 +0900

    feat: collect withdraw

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -175,6 +175,7 @@ class Transaction extends Model
     public function collectWithdraw()
     {
         return $this->hasMany(CollectWithdraw::class, 'mcht_id', 'mcht_id')
+            ->whereNotNull('mcht_settle_id')
             ->orderby('id', 'desc')
             ->select();
     }
