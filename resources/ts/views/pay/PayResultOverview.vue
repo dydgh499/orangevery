71a07f9d37dad9015fe59e71d6aba6863e0b2711
dydgh499 <dydgh499@naver.com>
2024-01-22 16:16:57 +0900

    fix: pay result over view

--- resources/ts/views/pay/PayResultOverview.vue
+++ resources/ts/views/pay/PayResultOverview.vue
@@ -12,90 +12,87 @@ interface Props {
 const props = defineProps<Props>()
 const salesslip = ref()
 
+onMounted(() => {
+    salesslip.value.show(props.sale_slip)
+})
 </script>
 <template>
     <section>
         <VCard rounded>
             <VCardText>
-                <VRow class="match-height">
-                    <VCol cols="12" md="12" class="d-flex justify-center align-center">
-                        <div style="width: 700px;">
+                <div id="pay-container">
+                    <div style="text-align: center;">
+                        <b v-if="props.result_cd != '0000'">
+                            결제를 실패하였습니다. 
                             <br>
-                            <div style="text-align: center;">
-                                <b v-if="props.result_cd != '0000'">
-                                    결제를 실패하였습니다. 
-                                    <br>
-                                    하단 실패사유를 확인해주세요.
-                                </b>
-                                <b v-else>
-                                    결제를 성공하였습니다 !
-                                    <br>
-                                    하단 결제정보를 확인해주세요.
-                                </b>
+                            하단 실패사유를 확인해주세요.
+                        </b>
+                        <b v-else>
+                            결제를 성공하였습니다 !
+                            <br>
+                            하단 결제정보를 확인해주세요.
+                        </b>
+                    </div>
+                    <VCard flat rounded>
+                        <VCardText>
+                            <VDivider />
+                            <div>
+                                <VTable v-if="result_cd != '0000'" style="margin: 3em 0; text-align: center;">
+                                    <tr>
+                                        <th class="padding">결과코드</th>
+                                        <td class="padding">{{ props.result_cd }}</td>
+                                    </tr>
+                                    <tr>
+                                        <th class="padding">에러 메세지</th>
+                                        <td class="padding"><span v-html="props.result_msg"></span></td>
+                                    </tr>
+                                </VTable>
+                                <VTable v-else style="margin: 3em 0; text-align: center;">
+                                    <tr>
+                                        <th class="padding">결과코드</th>
+                                        <td class="padding">{{ props.result_cd }}</td>
+                                    </tr>
+                                    <tr>
+                                        <th class="padding">응답 메세지</th>
+                                        <td class="padding">{{ props.result_msg }}</td>
+                                    </tr>
+                                    <tr>
+                                        <th class="padding">상품명</th>
+                                        <td class="padding">{{ props.sale_slip.item_name }}</td>
+                                    </tr>
+                                    <tr>
+                                        <th class="padding">금액</th>
+                                        <td class="padding">{{ props.sale_slip.amount }}</td>
+                                    </tr>
+                                    <tr>
+                                        <th class="padding">구매자명</th>
+                                        <td class="padding">{{ props.sale_slip.buyer_name }}</td>
+                                    </tr>
+                                    <tr>
+                                        <th class="padding">카드번호</th>
+                                        <td class="padding">{{ props.sale_slip.card_num }}</td>
+                                    </tr>
+                                    <tr>
+                                        <th class="padding">승인번호</th>
+                                        <td class="padding">{{ props.sale_slip.appr_num }}</td>
+                                    </tr>
+                                    <tr>
+                                        <th class="padding">매입사</th>
+                                        <td class="padding">{{ props.sale_slip.acquirer }}</td>
+                                    </tr>
+                                    <tr>
+                                        <th class="padding">발급사</th>
+                                        <td class="padding">{{ props.sale_slip.issuer }}</td>
+                                    </tr>
+                                    <tr>
+                                        <th class="padding">할부기간</th>
+                                        <td class="padding">{{ installments.find(obj => obj.id == props.sale_slip.installment)?.title }}</td>
+                                    </tr>
+                                </VTable>
                             </div>
-                            <VCard flat rounded>
-                                <VCardText>
-                                    <VDivider />
-                                    <div>
-                                        <VTable v-if="result_cd != '0000'" style="margin: 3em 0; text-align: center;">
-                                            <tr>
-                                                <th class="padding">결과코드</th>
-                                                <td class="padding">{{ props.result_cd }}</td>
-                                            </tr>
-                                            <tr>
-                                                <th class="padding">에러 메세지</th>
-                                                <td class="padding"><span v-html="props.result_msg"></span></td>
-                                            </tr>
-                                        </VTable>
-                                        <VTable v-else style="margin: 3em 0; text-align: center;">
-                                            <tr>
-                                                <th class="padding">결과코드</th>
-                                                <td class="padding">{{ props.result_cd }}</td>
-                                            </tr>
-                                            <tr>
-                                                <th class="padding">응답 메세지</th>
-                                                <td class="padding">{{ props.result_msg }}</td>
-                                            </tr>
-                                            <tr>
-                                                <th class="padding">상품명</th>
-                                                <td class="padding">{{ props.sale_slip.item_name }}</td>
-                                            </tr>
-                                            <tr>
-                                                <th class="padding">금액</th>
-                                                <td class="padding">{{ props.sale_slip.amount }}</td>
-                                            </tr>
-                                            <tr>
-                                                <th class="padding">구매자명</th>
-                                                <td class="padding">{{ props.sale_slip.buyer_name }}</td>
-                                            </tr>
-                                            <tr>
-                                                <th class="padding">카드번호</th>
-                                                <td class="padding">{{ props.sale_slip.card_num }}</td>
-                                            </tr>
-                                            <tr>
-                                                <th class="padding">승인번호</th>
-                                                <td class="padding">{{ props.sale_slip.appr_num }}</td>
-                                            </tr>
-                                            <tr>
-                                                <th class="padding">매입사</th>
-                                                <td class="padding">{{ props.sale_slip.acquirer }}</td>
-                                            </tr>
-                                            <tr>
-                                                <th class="padding">발급사</th>
-                                                <td class="padding">{{ props.sale_slip.issuer }}</td>
-                                            </tr>
-                                            <tr>
-                                                <th class="padding">할부기간</th>
-                                                <td class="padding">{{ installments.find(obj => obj.id == props.sale_slip.installment)?.title }}</td>
-                                            </tr>
-                                        </VTable>
-                                    </div>
-                                    <VDivider />
-                                </VCardText>
-                            </VCard>
-                        </div>
-                    </VCol>
-                </VRow>
+                        </VCardText>
+                    </VCard>
+                </div>
             </VCardText>
         </VCard>
         <SalesSlipDialog ref="salesslip" :pgs="pgs"/>
