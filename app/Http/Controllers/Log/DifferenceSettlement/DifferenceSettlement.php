5c07e5ea21e5a9f8dfd0a59a5384ab1dbaab1fc2
dydgh499 <dydgh499@naver.com>
2024-03-06 18:17:22 +0900

    feat:test

--- app/Http/Controllers/Log/DifferenceSettlement/DifferenceSettlement.php
+++ app/Http/Controllers/Log/DifferenceSettlement/DifferenceSettlement.php
@@ -74,17 +74,16 @@ class DifferenceSettlement
             if(count($mcht_trans) > 0)
             {
                 $_mid = $pmid_mode ? $mcht_trans[0]->p_mid : $mid;
-                $business_num = str_replace('-', '', $mcht_trans[0]->business_num);
+                if($_mid === "")
+                    continue;
 
-                if($business_num !== "" && $_mid !== "")
-                {
-                    $header = $this->setHeaderRecord($_mid);
-                    [$data_records, $count, $amount] = $this->service->setDataRecord($mcht_trans, $this->brand['business_num']);
-                    $total  = $this->setTotalRecord($count, $amount);
+                $header = $this->setHeaderRecord($_mid);
+                [$data_records, $count, $amount] = $this->service->setDataRecord($mcht_trans, $this->brand['business_num']);
+                $total  = $this->setTotalRecord($count, $amount);
 
-                    $full_record .= $header.$data_records.$total;
-                    $total_count += $count;    
-                }
+                $full_record .= $header.$data_records.$total;
+                $total_count += $count;    
+                
             }
         }
         if($this->service_name === 'danal')
