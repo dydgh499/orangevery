628166c30e908769088a41342cf336454b5847dc
dydgh499 <dydgh499@naver.com>
2024-03-14 16:40:17 +0900

    test:test

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -174,7 +174,7 @@ class AuthController extends Controller
         if($res['body']['result'] === 100)
             return $this->response(1, ['message'=> $res['body']['message']]);
         else
-            return $this->extendResponse(1999, $res['body']['message']);
+            return $this->response(1999, ['message'=> $res['body']['message']]);
     }
 }
 
