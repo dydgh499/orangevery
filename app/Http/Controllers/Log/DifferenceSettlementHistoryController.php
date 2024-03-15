1d5c974a30af098cdc1148f5f724dacae33cb7a5
dydgh499 <dydgh499@naver.com>
2024-03-15 14:34:55 +0900

    test:test

--- app/Http/Controllers/Log/DifferenceSettlementHistoryController.php
+++ app/Http/Controllers/Log/DifferenceSettlementHistoryController.php
@@ -274,15 +274,17 @@ class DifferenceSettlementHistoryController extends Controller
                 ->where('payment_gateways.pg_type', $brand->pg_type)
                 ->where('transactions.brand_id', $brand->brand_id)
                 ->where(function ($query) use ($yesterday) {
-                    $query->where('transactions.trx_dt', '<=', $yesterday)
-                        ->where('transactions.trx_dt', '>=', '2024-02-28')
-                        ->where('transactions.trx_dt', '!=', '2024-03-06')
-                        ->where('transactions.is_cancel', false);
-                })->orWhere(function ($query) use ($yesterday) {
-                    $query->where('transactions.cxl_dt', '<=', $yesterday)
-                    ->where('transactions.cxl_dt', '>=', '2024-02-28')
-                    ->where('transactions.cxl_dt', '!=', '2024-03-06')
-                    ->where('transactions.is_cancel', true);
+                    $query->where(function ($query) use ($yesterday) {                        
+                        $query->where('transactions.trx_dt', '<=', $yesterday)
+                            ->where('transactions.trx_dt', '>=', '2024-02-28')
+                            ->where('transactions.trx_dt', '!=', '2024-03-06')
+                            ->where('transactions.is_cancel', false);
+                    })->orWhere(function ($query) use ($yesterday) {
+                        $query->where('transactions.cxl_dt', '<=', $yesterday)
+                        ->where('transactions.cxl_dt', '>=', '2024-02-28')
+                        ->where('transactions.cxl_dt', '!=', '2024-03-06')
+                        ->where('transactions.is_cancel', true);
+                    });
                 })
                 ->get(['transactions.*', 'merchandises.business_num', 'payment_modules.p_mid']);
 
