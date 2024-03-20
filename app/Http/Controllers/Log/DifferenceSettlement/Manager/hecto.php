9758fac5da448268d77f4c1e63c9448bde37b8aa
dydgh499 <dydgh499@naver.com>
2024-03-20 15:31:10 +0900

    test:test

--- app/Http/Controllers/Log/DifferenceSettlement/Manager/hecto.php
+++ app/Http/Controllers/Log/DifferenceSettlement/Manager/hecto.php
@@ -125,10 +125,11 @@ class hecto implements DifferenceSettlementInterface
             else
             {
                 echo json_encode([
-                    'trans_id'   => $add_field,
+                    'trans_id'   => (int)$add_field,
                     'req_dt'    => $req_dt,
                     'settle_dt' => $settle_dt,
                 ]);
+                echo "\n";
             }
         }
         return $records;
