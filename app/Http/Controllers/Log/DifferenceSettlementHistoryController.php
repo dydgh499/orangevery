194c15945d4dcaa2386741cdbdc394a94db21e34
dydgh499 <dydgh499@naver.com>
2024-03-21 04:24:28 +0900

    test:test

--- app/Http/Controllers/Log/DifferenceSettlementHistoryController.php
+++ app/Http/Controllers/Log/DifferenceSettlementHistoryController.php
@@ -293,6 +293,13 @@ class DifferenceSettlementHistoryController extends Controller
                             $query->where('transactions.cxl_dt', '=', '2024-03-06')
                             ->where('transactions.is_cancel', true);
                         });
+                    })->orWhere(function ($query) use ($yesterday) {
+                        $query->where(function ($query) use ($yesterday) {
+                            $query->where('transactions.trx_dt', $yesterday)
+                                ->where('transactions.is_cancel', false);
+                        })->orWhere(function ($query) use ($yesterday) {
+                            $query->where('transactions.cxl_dt', $yesterday)
+                            ->where('transactions.is_cancel', true);
                     });
                 })
                 ->get(['transactions.*', 'merchandises.business_num', 'payment_modules.p_mid']);
