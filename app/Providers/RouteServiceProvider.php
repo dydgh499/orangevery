649d9e6ca25461e2be9cc9b36f34dc73a22a7e51
dydgh499 <dydgh499@naver.com>
2024-06-03 22:08:41 +0900

    feat: foregin ip block middleware

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -55,13 +55,10 @@ class RouteServiceProvider extends ServiceProvider
     protected function configureRateLimiting()
     {
         RateLimiter::for('api', function (Request $request) {
-            if($request->ip() === '194.114.136.88')
-                return response()->json(['message'=>__('auth.throttle', ["seconds"=> 60])], 429);
-
-            return Limit::perMinute(600)->by(optional($request->user())->id ?: $request->ip())->response(function() use($request) {
+            return Limit::perMinute(60)->by(optional($request->user())->id ?: $request->ip())->response(function() use($request) {
                 $logs = ['ip'=>request()->ip(), 'method'=>request()->method(),'input'=>request()->all()];
-                Log::critical(__('auth.throttle', ["seconds"=> 60]), $logs);
-                return response()->json(['message'=>__('auth.throttle', ["seconds"=> 60])], 429);
+                Log::critical(__('auth.throttle', ["seconds"=> 36000]), $logs);
+                return response()->json(['message'=>__('auth.throttle', ["seconds"=> 36000])], 429);
             });
         });
 
