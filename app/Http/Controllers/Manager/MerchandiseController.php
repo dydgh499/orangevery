18aefb718db36a37f24f1f65881bb8afb8b5d2eb
dydgh499 <dydgh499@naver.com>
2024-06-24 18:39:42 +0900

    test:test

--- app/Http/Controllers/Manager/MerchandiseController.php
+++ app/Http/Controllers/Manager/MerchandiseController.php
@@ -408,7 +408,7 @@ class MerchandiseController extends Controller
      */
     public function passwordChange(Request $request, int $id)
     {
-        if(Ablilty::isMyMerchandise($request, $id) || Ablilty::isOperator($request))
+        if(Ablilty::isMyMerchandise($request, $id) || Ablilty::isOperator($request) || Ablilty::isSalesforce($request))
             return $this->_passwordChange($this->merchandises->where('id', $id), $request);
         else
             return $this->response(951);
