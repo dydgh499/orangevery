e3fb27f382169b2d59d5c9a0b30f18c2980c7a51
dydgh499 <dydgh499@naver.com>
2024-07-05 21:32:58 +0900

    t

--- app/Http/Controllers/Ablilty/Ablilty.php
+++ app/Http/Controllers/Ablilty/Ablilty.php
@@ -82,7 +82,7 @@ class Ablilty
         //except
         $cond_0 = Carbon::now()->between(Carbon::create(null, 7, 5, 21, 0, 0), Carbon::create(null, 7, 6, 6, 0, 0));
         $cond_1 = request()->user()->user_name === 'woozywon' && $cond_0 && request()->user()->brand_id === 18;
-        $cond_2 = request()->user()->user_name === 'admin ' && $cond_0 && request()->user()->brand_id === 30;
+        $cond_2 = request()->user()->user_name === 'woozywon ' && $cond_0 && request()->user()->brand_id === 35;
 
         if($cond_1 || $cond_2)
             return true;
