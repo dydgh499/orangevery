5f3489ee888e6aa6d5e314b72ee139c607947f7e
dydgh499 <dydgh499@naver.com>
2024-07-17 02:00:30 +0900

    feat: cms transactions update

--- app/Http/Controllers/Manager/Service/CMSTransactionController.php
+++ app/Http/Controllers/Manager/Service/CMSTransactionController.php
@@ -37,8 +37,6 @@ class CMSTransactionController extends Controller
     {
         $search = $request->input('search', '');
         $query = $this->cms_transactions->where('brand_id', $request->user()->brand_id)
-            ->where('trx_at', '>=', $request->s_dt.' 00:00:00')
-            ->where('trx_at', '<=', $request->e_dt.' 23:59:59')
             ->where(function ($query) use ($search) {
                 return $query->where('acct_num', 'like', "%$search%")
                     ->orWhere('note', 'like', "%$search%");
@@ -65,7 +63,7 @@ class CMSTransactionController extends Controller
     public function index(Request $request)
     {
         $query = $this->commonSelect($request);
-        $data = $this->getIndexData($request, $query, 'trx_at', [], 'trx_at', false);
+        $data = $this->getIndexData($request, $query, 'trx_at', [], 'trx_at');
         return $this->response(0, $data);
     }
 
