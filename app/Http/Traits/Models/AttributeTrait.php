f339bfd63be58bd5202be08954d830e0e2041b29
dydgh499 <dydgh499@naver.com>
2024-07-26 18:35:46 +0900

    feat: test

--- app/Http/Traits/Models/AttributeTrait.php
+++ app/Http/Traits/Models/AttributeTrait.php
@@ -22,14 +22,55 @@ trait AttributeTrait
         return $date->format("Y-m-d H:i:s");
     }
 
+
     private function toS3PrivateLink($link)
     {
         $client = Storage::disk('s3')->getClient();
         $command = $client->getCommand('GetObject', [
             'Bucket' => Config::get('filesystems.disks.s3.bucket'),
-            'Key'    => str_replace(Config::get('filesystems.disks.s3.url'), '', $link)
+            'Key'    => str_replace(Config::get('filesystems.disks.s3.url').'/', '', $link)
         ]);
         $request = $client->createPresignedRequest($command, '+10 minutes');
-        return Config::get('filesystems.disks.s3.url').$request->getUri();
+        return $request->getUri();
+    }
+
+    private function isS3Img($value)
+    {
+        return env('FILESYSTEM_DISK') === 's3' && strpos($value, 'amazonaws.com') !== false;
+    }
+
+    protected function ProfileImg() : Attribute
+    {
+        return new Attribute(
+            get: fn ($value) => $this->isS3Img($value) ? $this->toS3PrivateLink($value) : $value
+        );
+    }
+
+    protected function ContractImg() : Attribute
+    {
+        return new Attribute(
+            get: fn ($value) => $this->isS3Img($value) ? $this->toS3PrivateLink($value) : $value
+        );
+    }
+
+    protected function BsinLicImg() : Attribute
+    {
+        return new Attribute(
+            get: fn ($value) => $this->isS3Img($value) ? $this->toS3PrivateLink($value) : $value
+        );
+    }
+
+    protected function IdImg() : Attribute
+    {
+        return new Attribute(
+            get: fn ($value) => $this->isS3Img($value) ? $this->toS3PrivateLink($value) : $value
+        );
+    }
+
+    protected function PassbookImg() : Attribute
+    {
+        return new Attribute(
+            get: fn ($value) => $this->isS3Img($value) ? $this->toS3PrivateLink($value) : $value
+        );
     }
 }
