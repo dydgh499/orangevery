57f068ed00125426d19ce08b20cb444bfe7560d4
dydgh499 <dydgh499@naver.com>
2025-04-01 15:55:48 +0900

    feat: merchandise sub name

--- app/Http/Middleware/CheckUserAgent.php
+++ app/Http/Middleware/CheckUserAgent.php
@@ -35,6 +35,7 @@ class CheckUserAgent
             'Microsoft Windows Network Diagnostics',
             'GoogleAssociationService',
             'WebInfoUtil/1.0',
+            'com.apple.WebKit.Networking',
         ];
         $user_agent = $request->header('User-Agent');
         // User-Agent가 정상적인 브라우저 목록에 포함되는지 검사
