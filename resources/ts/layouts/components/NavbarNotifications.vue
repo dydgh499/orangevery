5667b9ac1f85cb560555a312c2b38aec37dff748
29qkrdlrb <qkrdlrbq@gmail.com>
2025-07-04 04:19:04 +0900

    fix: hide secure report

--- resources/ts/layouts/components/NavbarNotifications.vue
+++ resources/ts/layouts/components/NavbarNotifications.vue
@@ -1,18 +1,16 @@
 <script lang="ts" setup>
 import DetailWorkStatusDialog from '@/layouts/dialogs/utils/DetailWorkStatusDialog.vue'
-import SecureReportDialog from '@/layouts/dialogs/utils/SecureReportDialog.vue'
-
-import { axios } from '@axios'
+//import SecureReportDialog from '@/layouts/dialogs/utils/SecureReportDialog.vue'
 import Notifications from '@core/components/Notifications.vue'
 import type { Notification } from '@layouts/types'
 
 const errorHandler = <any>(inject('$errorHandler'))
 const notifications = ref(<Notification[]>[])
 const detailWorkStatusDialog = ref()
-const secureReportDialog = ref(false)
+//const secureReportDialog = ref(false)
 const is_login = ref(true)
 
-provide('detailWorkStatusDialog', detailWorkStatusDialog)
+// provide('detailWorkStatusDialog', detailWorkStatusDialog)
 </script>
 
 <template>
@@ -20,7 +18,9 @@ provide('detailWorkStatusDialog', detailWorkStatusDialog)
         <Notifications :notifications="notifications" />
         <template v-if="is_login">
             <Suspense>
+                <!--
                 <SecureReportDialog ref="secureReportDialog"/>
+                -->
             </Suspense>
             <DetailWorkStatusDialog ref="detailWorkStatusDialog"/>
         </template>
