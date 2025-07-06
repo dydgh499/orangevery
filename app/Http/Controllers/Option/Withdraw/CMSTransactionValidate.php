a7df2018e33735c2ed56cb09c2f7fa0b907caa86
29qkrdlrb <qkrdlrbq@gmail.com>
2025-07-06 23:35:02 +0900

    fix: remove useless code

--- app/Http/Controllers/Option/Withdraw/CMSTransactionValidate.php
+++ app/Http/Controllers/Option/Withdraw/CMSTransactionValidate.php
@@ -8,15 +8,6 @@ use App\Models\Transaction;
 
 class CMSTransactionValidate
 {
-    static public function updateWithdraw($last_id, $result_code, $message)
-    {
-        $params = [
-            'result_code'=> $result_code,
-            'message' => $message,
-        ];
-        return CMSTransaction::where('id', $last_id)->update($params);
-    }
-
     static public function cancelValidate($history)
     {
         return Transaction::where('ori_trx_id', $history['trx_id'])
