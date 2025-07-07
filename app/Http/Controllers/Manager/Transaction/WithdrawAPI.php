fc636d8005bcc5c1c4f9414b00a3d5014f00df94
29qkrdlrb <qkrdlrbq@gmail.com>
2025-07-07 10:40:28 +0900

    fix: batch remove

--- app/Http/Controllers/Manager/Transaction/WithdrawAPI.php
+++ app/Http/Controllers/Manager/Transaction/WithdrawAPI.php
@@ -30,8 +30,6 @@ class WithdrawAPI extends WithdrawTest
     static public function ownerCheck($data)
     {
         $url = env('WITHDRAW_URL', 'http://localhost:81').'/api/v2/realtimes/owner-check';
-            return Comm::post($url, $data, []);
-            /*
         if(Ablilty::isAppLocal())
         {
             return [
@@ -41,8 +39,8 @@ class WithdrawAPI extends WithdrawTest
         }
         else
         {
+            return Comm::post($url, $data, []);
         }
-        */
     }
     
     static public function bookWithdraw($data)
