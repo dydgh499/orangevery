40d5ca2bf2b6971e8c5124fe6c7f4deceffb395e
29qkrdlrb <qkrdlrbq@gmail.com>
2025-07-07 15:43:47 +0900

    fix: roll back comments

--- app/Http/Controllers/Manager/Transaction/WithdrawAPI.php
+++ app/Http/Controllers/Manager/Transaction/WithdrawAPI.php
@@ -30,8 +30,6 @@ class WithdrawAPI extends WithdrawTest
     static public function ownerCheck($data)
     {
         $url = env('WITHDRAW_URL', 'http://localhost:81').'/api/v2/realtimes/owner-check';
-            return Comm::post($url, $data, []);
-            /*
         if(Ablilty::isAppLocal())
         {
             return [
@@ -41,7 +39,7 @@ class WithdrawAPI extends WithdrawTest
         }
         else
         {
+            return Comm::post($url, $data, []);
         }
-        */
     }
 }
